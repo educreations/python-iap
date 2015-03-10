6d3fbf5597d2899fbd16cb7a5212145a47fe20c7
Chris Streeter <chris@chrisstreeter.com>
2015-03-09 17:51:03 -0700

    Supply author_email
    From publishing:
    > warning: check: missing meta-data: if 'author' supplied, 'author_email'
    > must be supplied too

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version='0.2',
     description='Python utilities for working with Apple In-App Purchases (IAP)',
     author='Educreations Engineering',
+    author_email='engineering@educreations.com',
     py_modules=['iap'],
     install_requires=['pycrypto', 'django', 'requests'],
 )
