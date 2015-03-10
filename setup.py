c15d93f0dea41b2b859da1141f7bf0c2fa1109dc
Chris Streeter <chris@chrisstreeter.com>
2015-03-09 17:48:09 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='iap',
-    version='0.1',
+    version='0.2',
     description='Python utilities for working with Apple In-App Purchases (IAP)',
     author='Educreations Engineering',
     py_modules=['iap'],
