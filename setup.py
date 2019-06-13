7fb00614983abf5c8e9f4e148e10fd9188800a67
Chris Streeter <chris@educreations.com>
2019-06-13 10:46:41 -0700

    Add exception code constants
    Also a bit of reformatting

--- setup.py
+++ setup.py
@@ -6,31 +6,29 @@ import sys
 from setuptools import setup
 
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py register sdist bdist_wheel upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py register sdist bdist_wheel upload")
     sys.exit()
 
 
 setup(
-    name='iap',
-    version='2.2.1',
-    description='Python utilities for working with Apple In-App Purchases (IAP)',
+    name="iap",
+    version="2.2.1",
+    description="Python utilities for working with Apple In-App Purchases (IAP)",
     license="MIT",
     keywords="iap appstore django",
-    author='Educreations Engineering',
-    author_email='engineering@educreations.com',
-    url='https://github.com/educreations/python-iap',
+    author="Educreations Engineering",
+    author_email="engineering@educreations.com",
+    url="https://github.com/educreations/python-iap",
     packages=["iap"],
     package_dir={"iap": "iap"},
     install_requires=[
-        'Django>=1.7',
-        'pyopenssl>=17.0.0',
-        'pyasn1',
-        'pyasn1_modules',
-        'requests',
+        "Django>=1.7",
+        "pyopenssl>=17.0.0",
+        "pyasn1",
+        "pyasn1_modules",
+        "requests",
     ],
-    tests_require=[
-        'pytest',
-        'pytest-django',
-    ],
+    extras_require={"test": ["pytest", "pytest-django", "responses"]},
+    tests_require=["iap[test]"],
 )
