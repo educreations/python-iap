866694938ed218f8a4dbaee6cd7704ba8cd118d1
Chris Streeter <chris@educreations.com>
2019-06-13 11:52:59 -0700

    Add more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,16 +1,36 @@
 import os
 
-from iap.utils import parse_receipt
+import pytest
+import responses
+
+from iap.exceptions import (
+    NoPurchasesException,
+    ReceiptValidationException,
+    RetryReceiptValidation,
+    APPSTORE_STATUS_INVALID_JSON,
+    APPSTORE_STATUS_MALFORMED_RECEIPT_DATA,
+    APPSTORE_STATUS_RECEIPT_AUTHENTICATION,
+    APPSTORE_STATUS_SHARED_SECRET_MISMATCH,
+    APPSTORE_STATUS_RECEIPT_SERVER_DOWN,
+    APPSTORE_STATUS_EXPIRED_SUBSCRIPTION,
+    APPSTORE_STATUS_TEST_ENVIRONMENT_RECEIPT,
+    APPSTORE_STATUS_PROD_ENVIRONMENT_RECEIPT,
+    APPSTORE_STATUS_UNAUTHORIZED_RECEIPT,
+    APPSTORE_STATUS_INTERNAL_DATA_ACCESS_ERROR_MIN,
+    APPSTORE_STATUS_INTERNAL_DATA_ACCESS_ERROR_MAX,
+)
+from iap.settings import PRODUCTION_VERIFICATION_URL, SANDBOX_VERIFICATION_URL
+from iap.utils import parse_receipt, validate_receipt_with_apple
+
 
 RECEIPT_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "receipt.bin")
 
+with open(RECEIPT_FILE, "rb") as r:
+    assert r is not None
+    receipt_data = r.read()
+
 
 def test_parse_receipt():
-    with open(RECEIPT_FILE, "rb") as r:
-        receipt_data = r.read()
-
-    assert r is not None
-
     receipt_info = parse_receipt(receipt_data)
     assert receipt_info
     for key in (
@@ -33,3 +53,49 @@ def test_parse_receipt():
             "original_purchase_date",
         ):
             assert key in iap
+
+
+@responses.activate
+def test_validate_receipt_with_apple_requires_json():
+    responses.add(
+        responses.Response(method="POST", url=PRODUCTION_VERIFICATION_URL, body="")
+    )
+
+    with pytest.raises(ReceiptValidationException):
+        validate_receipt_with_apple(receipt_data)
+
+
+@responses.activate
+def test_validate_receipt_with_apple_bad_status():
+    statuses_to_exceptions = [
+        [APPSTORE_STATUS_INVALID_JSON, ReceiptValidationException],
+        [APPSTORE_STATUS_MALFORMED_RECEIPT_DATA, ReceiptValidationException],
+        [APPSTORE_STATUS_RECEIPT_AUTHENTICATION, ReceiptValidationException],
+        [APPSTORE_STATUS_SHARED_SECRET_MISMATCH, ReceiptValidationException],
+        [APPSTORE_STATUS_RECEIPT_SERVER_DOWN, RetryReceiptValidation],
+        [APPSTORE_STATUS_EXPIRED_SUBSCRIPTION, ReceiptValidationException],
+        [APPSTORE_STATUS_TEST_ENVIRONMENT_RECEIPT, ReceiptValidationException],
+        [APPSTORE_STATUS_PROD_ENVIRONMENT_RECEIPT, ReceiptValidationException],
+        [APPSTORE_STATUS_UNAUTHORIZED_RECEIPT, NoPurchasesException],
+        [21009, RetryReceiptValidation],
+        [APPSTORE_STATUS_INTERNAL_DATA_ACCESS_ERROR_MIN, RetryReceiptValidation],
+        [APPSTORE_STATUS_INTERNAL_DATA_ACCESS_ERROR_MAX, RetryReceiptValidation],
+    ]
+
+    for status, exception in statuses_to_exceptions:
+        with responses.RequestsMock(assert_all_requests_are_fired=False) as rsps:
+            rsps.add(
+                responses.Response(
+                    method="POST",
+                    url=PRODUCTION_VERIFICATION_URL,
+                    json={"status": status},
+                )
+            )
+            rsps.add(
+                responses.Response(
+                    method="POST", url=SANDBOX_VERIFICATION_URL, json={"status": status}
+                )
+            )
+
+            with pytest.raises(exception):
+                validate_receipt_with_apple(receipt_data)
